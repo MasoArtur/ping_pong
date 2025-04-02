5235f0c01f4c94582ea3b62dcaefe010c6a7397c
Artur Maso <masoartur@gmail.com>
2025-04-02 15:57:54 -0300

    Tarefa
    Fazer um código básico em javascript e rodar no terminal utilizando a expansão run code

---
+++ index.js
@@ -0,0 +1,18 @@
+var number1 = 10;
+number1 = parseFloat(number1);
+
+var number2 = 5;
+number2 = parseFloat(number2);
+
+var soma = (number1 + number2);
+
+var diminuir = (number1 - number2);
+
+var multiplicar = (number1 * number2);
+
+var dividir = (number1 / number2);
+
+console.log("A resposta é: " + soma);
+console.log("A resposta é: " + diminuir);
+console.log("A resposta é: " + multiplicar);
+console.log("A resposta é: " + dividir);
